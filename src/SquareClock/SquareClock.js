a9f65ffeefe1dea3c0e58917c6fba2a7009f646c
Stefan Wille <github@stefanwille.com>
2017-12-22 21:50:31 +0100

    Make layout ipad mini ready

--- src/SquareClock/SquareClock.js
+++ src/SquareClock/SquareClock.js
@@ -9,32 +9,20 @@ const Character = ({ character, glowing }) => (
   <div className={classNames("Character", { glowing })}>{character}</div>
 );
 
-const Display = ({ readout }) => {
-  const width = readout[0].length;
-  return (
-    <div
-      className="SquareClock"
-      style={{
-        gridTemplateColumns: `repeat(${width}, 1fr)`
-      }}
-    >
-      {readout.map((line, rowIndex) =>
-        line.split("").map((readoutCharacter, columnIndex) => {
-          const displayCharacter = DISPLAY[rowIndex][columnIndex];
-          const glowing = readoutCharacter !== " ";
-          const key = `${rowIndex}-${columnIndex}`;
-          return (
-            <Character
-              character={displayCharacter}
-              glowing={glowing}
-              key={key}
-            />
-          );
-        })
-      )}
-    </div>
-  );
-};
+const Display = ({ readout }) => (
+  <div className="SquareClock">
+    {readout.map((line, rowIndex) =>
+      line.split("").map((readoutCharacter, columnIndex) => {
+        const displayCharacter = DISPLAY[rowIndex][columnIndex];
+        const glowing = readoutCharacter !== " ";
+        const key = `${rowIndex}-${columnIndex}`;
+        return (
+          <Character character={displayCharacter} glowing={glowing} key={key} />
+        );
+      })
+    )}
+  </div>
+);
 
 export default class SquareClock extends Component {
   constructor(props) {

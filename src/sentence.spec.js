3af4c2a1abf00ab1377fc75c988e42c9ed9401de
Stefan Wille <github@stefanwille.com>
2017-12-25 14:06:48 +0100

    Improve spec

--- src/sentence.spec.js
+++ src/sentence.spec.js
@@ -1,50 +1,44 @@
 //@ts-check
-const {
-  mapDateToSentence,
-  mapTimeToSentence,
-  toNaturalLanguage
-} = require("./sentence");
+const { mapDateToSentence, mapTimeToSentence, toNaturalLanguage } = require('./sentence');
 
-describe("mapDateToSentence()", () => {
-  it("takes a date and maps it to a display time", () => {
-    const date = new Date("2017-12-16T21:10:09+01:00");
+describe('mapDateToSentence()', () => {
+  it('takes a date and maps it to a display time', () => {
+    const date = new Date('2017-12-16T21:10:09+01:00');
     expect(mapDateToSentence(date)).toEqual({
-      minutes: "ZEHN",
-      relation: "NACH",
-      hours: "NEUN"
+      minutes: 'ZEHN',
+      relation: 'NACH',
+      hours: 'NEUN',
     });
   });
 });
 
-describe("mapTimeToSentence()", () => {
-  describe("with a time", () => {
-    it("maps a 12 hour time to a sentence", () => {
-      expect(mapTimeToSentence("9:10")).toEqual({
-        minutes: "ZEHN",
-        relation: "NACH",
-        hours: "NEUN"
+describe('mapTimeToSentence()', () => {
+  describe('with a time', () => {
+    it('maps a 12 hour time to a sentence', () => {
+      expect(mapTimeToSentence('9:10')).toEqual({
+        minutes: 'ZEHN',
+        relation: 'NACH',
+        hours: 'NEUN',
       });
     });
 
-    it("can be converted to natural language", () => {
-      expect(toNaturalLanguage(mapTimeToSentence("9:10"))).toBe(
-        "ZEHN NACH NEUN"
-      );
+    it('can be converted to natural language', () => {
+      expect(toNaturalLanguage(mapTimeToSentence('9:10'))).toBe('ZEHN NACH NEUN');
     });
   });
 
-  describe("all hours", () => {
+  describe('all hours', () => {
     for (let hours = 1; hours <= 12; hours += 1) {
-      const time = `${hours}:05`;
+      const time = `${String(hours).padStart(2, '0')}:05`;
       it(`maps ${time}`, () => {
         expect(toNaturalLanguage(mapTimeToSentence(time))).toMatchSnapshot();
       });
     }
   });
 
-  describe("all minutes", () => {
-    for (let minutes = 1; minutes < 60; minutes += 1) {
-      const time = `3:${minutes}`;
+  describe('all minutes', () => {
+    for (let minutes = 0; minutes < 60; minutes += 1) {
+      const time = `03:${String(minutes).padStart(2, '0')}`;
       it(`maps ${time}`, () => {
         expect(toNaturalLanguage(mapTimeToSentence(time))).toMatchSnapshot();
       });
